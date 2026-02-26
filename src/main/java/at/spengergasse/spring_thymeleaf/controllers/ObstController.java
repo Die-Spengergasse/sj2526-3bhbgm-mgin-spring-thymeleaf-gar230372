83667153c1c4591948351b80138aef5b1df2f889
gar230372 <gar230372@spengergasse.at>
2026-02-26 15:59:49 +0100

    kommentare hinzugefügt

--- src/main/java/at/spengergasse/spring_thymeleaf/controllers/ObstController.java
+++ src/main/java/at/spengergasse/spring_thymeleaf/controllers/ObstController.java
@@ -15,40 +15,39 @@ public class ObstController {
         this.obstRepository = obstRepository;
     }
 
+    // Alle Obstsorten auflisten
     @GetMapping ("/list")
     public String obst(Model model) {
         model.addAttribute("obst", obstRepository.findAll());
         return "obstlist";
     }
 
+    // Neue Obstsorte hinzufügen
     @GetMapping ("/add")
     public String addObst(Model model) {
         model.addAttribute("obst", new Obst());
         return "add_obst";
     }
 
+    // nach hinzufügen vom obst wird es gespeichert und man wird wieder auf die seite mit der liste zurückgeleitet
     @PostMapping("/add")
     public String addObst(@ModelAttribute ("obst") Obst obst) {
         obstRepository.save(obst);
         return "redirect:/obst/list";
     }
 
-    /*@GetMapping("/edit")
-    public String editObst(Model model, Obst obst){
-        model.addAttribute("id", obstRepository.findById(obst.getId()));
-        return "add_obst";
-    }*/
-
+    //Obst bearbeiten (nach der Erstellung)
     @GetMapping("/edit")
     public String editObst(@RequestParam int id, Model model) {
-        Obst obst = obstRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid obst Id:" + id));
+        Obst obst = obstRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid obst Id:" + id)); //sucht das obst mit der id, damit kein neues erstellt wird und speichert die id in dem objekt "obst"
         model.addAttribute("obst", obst);
         return "add_obst";
     }
 
+    //Obst löschen
     @GetMapping("/delete")
     public String deleteObst(Obst obst) {
-        obstRepository.deleteById(obst.getId());
+        obstRepository.deleteById(obst.getId()); //Löscht das Obst anhand der Id (daran wird es gefunden damit das richtige gelöscht wird)
         return "redirect:/obst/list";
     }
 
